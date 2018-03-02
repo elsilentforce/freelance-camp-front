6f8004848c55bf9b4ed88d52203d5bb160fe5ee3
Patricio Tapia <native.devil@gmail.com>
2018-03-02 15:44:09 -0300

    ADD: Proposal-List component

---
+++ src/app/proposal/proposal-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  moduleId: module.id,
+  templateUrl: 'proposal-list.component.html',
+  selector: 'proposal-list'
+})
+
+export class ProposalListComponent {}
