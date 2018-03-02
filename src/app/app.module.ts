c399ce06cfbc9df50f18700a66aff540b4e03bdb
Patricio Tapia <native.devil@gmail.com>
2018-03-01 21:25:16 -0300

    FIX: AppComponent reference

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './homepage.component'
+import { HomePageComponent } from './homepage/homepage.component'
 
 
 @NgModule({
   declarations: [
+    AppComponent,
     HomePageComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [HomePageComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
