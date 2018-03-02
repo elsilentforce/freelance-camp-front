8fd59c453964f6ec354ae64529ff8478bda002d7
Patricio Tapia <native.devil@gmail.com>
2018-03-01 22:15:43 -0300

    ADD: HomePage view

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'homepage',
-  template: '<h1>Freelance Camp Dashboard</h1>'
+  templateUrl: 'homepage.component.html'
 })
 
 export class HomePageComponent {
