4434cd3af8ae89869c91e341cf433f85b6656f93
Patricio Tapia <native.devil@gmail.com>
2018-03-01 21:54:38 -0300

    ADD: AppRouting Decorator

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,3 +7,10 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent }
 ]
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+
+export class AppRoutingModule {}
