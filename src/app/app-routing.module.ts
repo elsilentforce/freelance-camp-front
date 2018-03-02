43b5ec912e7aa1f24c93327290a485b3fd2043fa
Patricio Tapia <native.devil@gmail.com>
2018-03-01 21:51:58 -0300

    ADD: AppRouting

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+
+import { HomePageComponent } from './homepage/homepage.component'
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent }
+]
