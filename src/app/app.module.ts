3fc099becbd6be260274af57e0d20d7efcdb8929
Patricio Tapia <native.devil@gmail.com>
2018-03-02 15:50:45 -0300

    ADD: ProposalListComponent on AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './homepage/homepage.component'
 import { DocumentsComponent } from './documents/documents.component'
+import { ProposalListComponent } from './proposal/proposal-list.component'
 
 import { AppRoutingModule } from './app-routing.module'
 
@@ -13,7 +14,8 @@ import { AppRoutingModule } from './app-routing.module'
   declarations: [
     AppComponent,
     HomePageComponent,
-    DocumentsComponent
+    DocumentsComponent,
+    ProposalListComponent
   ],
   imports: [
     BrowserModule,
