418901070d6d36a300241d23d9a1609fe5d23a81
Patricio Tapia <native.devil@gmail.com>
2018-03-03 20:50:46 -0300

    ADD: Proposal Class

---
+++ src/app/proposal/proposal.ts
@@ -0,0 +1,14 @@
+export class Proposal {
+  constructor(
+    public id?: number,
+    public customer?: string,
+    public portfolio_url: string = 'http://',
+    public tools?: string,
+    public estimated_hours?: number,
+    public hourly_rate?: number,
+    public weeks_to_complete?: number,
+    public client_email?: string
+  ) {}
+}
+
+// 10, 'ASDF Company', 'http://testuser.portfolio', 'Ruby on Rails', 40, 1, 'testuser@testmail.test'
