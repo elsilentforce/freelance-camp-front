c6f65879a69059e9520ece499c9d3932a536e6a2
Patricio Tapia <native.devil@gmail.com>
2018-03-04 01:07:06 -0300

    ADD: Proposal on ProposalList

--- src/app/proposal/proposal-list.component.ts
+++ src/app/proposal/proposal-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { Proposal } from './proposal'
 
 @Component({
   moduleId: module.id,
@@ -6,4 +7,7 @@ import { Component } from '@angular/core'
   selector: 'proposal-list'
 })
 
-export class ProposalListComponent {}
+export class ProposalListComponent {
+  proposalOne: Proposal = new Proposal(10, 'ASDF Company', 'http://testuser.portfolio', 'Ruby on Rails', 40, 100, 1, 'testuser@testmail.test')
+
+}
