9b9281fab7620b26d216cdd7933e2c7a05a2a7a9
Patricio Tapia <native.devil@gmail.com>
2018-03-04 01:09:49 -0300

    ADD: Proposals array

--- src/app/proposal/proposal-list.component.ts
+++ src/app/proposal/proposal-list.component.ts
@@ -9,5 +9,12 @@ import { Proposal } from './proposal'
 
 export class ProposalListComponent {
   proposalOne: Proposal = new Proposal(10, 'ASDF Company', 'http://testuser.portfolio', 'Ruby on Rails', 40, 100, 1, 'testuser@testmail.test')
+  proposalTwo: Proposal = new Proposal(11, 'Another Company', 'http://testuser.portfolio', 'Ruby on Rails', 40, 100, 1, 'testuser@testmail.test')
+  proposalThree: Proposal = new Proposal(10, 'Not the same Company', 'http://testuser.portfolio', 'Ruby on Rails', 40, 100, 1, 'testuser@testmail.test')
 
+  proposals: Proposal[] = [
+    this.proposalOne,
+    this.proposalTwo,
+    this.proposalThree
+  ]
 }
