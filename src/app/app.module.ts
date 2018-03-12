0553eeead90412123624634a97fc7006cb8a4b05
Patricio Tapia <native.devil@gmail.com>
2018-03-12 15:58:13 -0300

    ADD: Proposal show component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomePageComponent } from './homepage/homepage.component'
 import { DocumentsComponent } from './documents/documents.component'
 import { ProposalListComponent } from './proposal/proposal-list.component'
 import { ProposalNewComponent } from './proposal/proposal-new.component'
+import { ProposalShowComponent } from './proposal/proposal-show.component'
 
 import { AppRoutingModule } from './app-routing.module'
 
@@ -18,7 +19,8 @@ import { AppRoutingModule } from './app-routing.module'
     HomePageComponent,
     DocumentsComponent,
     ProposalListComponent,
-    ProposalNewComponent
+    ProposalNewComponent,
+    ProposalShowComponent
   ],
   imports: [
     BrowserModule,
